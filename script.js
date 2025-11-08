d5b0cff49b8a1aabcb07acb6fd09dbdd55cb4a59
Shiroko4592 <runarunayeh@naver.com>
2025-11-08 14:09:47 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// 필요 시 설치 진행 중 로딩 표시 등 추가 가능
 document.getElementById('mwInstallForm')?.addEventListener('submit', function(){
     const result = document.getElementById('mwResult');
     if(result) result.innerText = "설치 진행 중...";
